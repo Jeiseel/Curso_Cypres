797165ac66efab61cc5daf39de16728009ec96f3
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2022-01-19 18:10:26 -0300

    Primeiro commit

---
+++ cypress/integration/arrow.spec.js
@@ -0,0 +1,13 @@
+it('primeiro teste', function () { })
+
+// function soma (a,b){
+//     return a + b;
+// }
+// const soma = ( a, b) => {
+//     return a + b;
+// }
+
+// const soma = a => a =a
+
+// const soma = () => 5 + 5
+// console.log(soma(1, 9))
