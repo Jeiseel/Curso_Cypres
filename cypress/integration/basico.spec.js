21cd3a35e6edb10487c82c30509ad22a26334015
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2022-01-31 15:30:10 -0300

    Debug e Pause

--- cypress/integration/basico.spec.js
+++ cypress/integration/basico.spec.js
@@ -1,12 +1,13 @@
 /// <Reference types="cypress" />
 
 describe('Cypress Basic', () => {
-    it('Visitando site e comparando o titulo', () => {
+    it.only('Visitando site e comparando o titulo', () => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+        //cy.pause() // execução com pausas planejadas 
         
-        cy.title()
-            .should('be.equal', 'Campo de Treinamento')
-            .and('contain', 'Campo')
+        cy.title().should('be.equal', 'Campo de Treinamento')
+        cy.title().should('contain', 'Campo')//.debug() pegar informações da pagina com detalhes.
     })
 
     it('Intereção com os elementos', () => {
