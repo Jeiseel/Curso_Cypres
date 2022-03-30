5235c70e5090f67eae65bb921f804b5bdd6edb79
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2022-03-30 10:27:21 -0300

    Elementos links e botões

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -9,4 +9,17 @@ describe('Elementos basicos', () => {
         cy.get('.facilAchar').should('contain', 'Cuidado')
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...' )
     })
+
+    it.only('Links', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
+        cy.reload()
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+
+    })
+    
 })
