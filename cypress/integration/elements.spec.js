1b41fec76033610013219cde5209b72b1f8ccc54
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2022-03-30 15:11:16 -0300

    Elementos basicos campos de texto

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -16,7 +16,7 @@ describe('Elementos basicos', () => {
         cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
     })
 
-    it('Links', () => {
+    it('Links', () => {// acessando links na pagina
         cy.get('[href="#"]').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
 
@@ -24,6 +24,29 @@ describe('Elementos basicos', () => {
         cy.get('#resultado').should('have.not.text', 'Voltou!')
         cy.contains('Voltar').click()
         cy.get('#resultado').should('have.text', 'Voltou!')
+
+    })
+
+    it.only('TextFields', () => { //escrevendo nos campos de texto
+        cy.get('#formNome').type('Teste automatizado com cypress')
+        cy.get('#formNome').should('have.value', 'Teste automatizado com cypress')
+
+        cy.get('#elementosForm\\:sugestoes') //- quando tiver um id contendo : ou '\' o cypress não entendo então antes da barra e do ponto usar duas barras '\\'
+            .type('TextArea Aqui!')
+            .should('have.value', 'TextArea Aqui!')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('Que nome de tabala grande da peste!')
+
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Testando Com Cypress 123{backspace}{backspace}')
+            .should('have.value', 'Testando Com Cypress 1')
+        
+        cy.get('#elementosForm\\:sugestoes') //- quando tiver um id contendo : ou '\' o cypress não entendo então antes da barra e do ponto usar duas barras '\\'
+            .clear()
+            .type('Esse texto aqui{selectall}Mas na verdade é esse texto', {delay:100} )
+            .should('have.value', 'Mas na verdade é esse texto')
+        
 
     })
 
