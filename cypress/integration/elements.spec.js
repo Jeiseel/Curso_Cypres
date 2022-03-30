918e02b7a2fc13cb86f0a8f448e847bf3462362f
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2022-03-30 09:30:42 -0300

    Elementos de Texto

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -6,5 +6,7 @@ describe('Elementos basicos', () => {
 
         cy.get('body').should('contain','Cuidado')
         cy.get('span').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...' )
     })
 })
