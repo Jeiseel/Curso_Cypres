62165ca30e513ea686ef4921b8054ed840aae090
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2022-04-26 11:08:53 -0300

    Popup

---
+++ cypress/integration/Popup.spec.js
@@ -0,0 +1,24 @@
+/// <Reference types="cypress" />
+
+describe('Trabalhando com Popup', () => {
+
+    it('Testando Popup diretamente', () => {
+        cy.visit('https://www.wcaquino.me/cypress/frame.html')
+        cy.get('#otherButton').click()
+        
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Click OK!') //Verifica se a mensagem exibida no alerta é igual a mensagem passada no parametro do equal
+            })
+
+    })
+
+    it.only('Verifica se o Popup foi invocado', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        cy.window().then(win =>{
+            cy.stub(win, 'open').as('winOpen')
+        })
+        cy.get('#buttonPopUp').click()
+        cy.get('@winOpen').should('be.called')
+    })
+
+})
