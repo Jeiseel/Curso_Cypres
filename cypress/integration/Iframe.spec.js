eb636d29e0ef680bd9290cdf818201ba338f21e8
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2022-04-26 10:01:01 -0300

    Testando frame diretamente

--- cypress/integration/Iframe.spec.js
+++ cypress/integration/Iframe.spec.js
@@ -8,12 +8,21 @@ describe('Trabalhando com Iframes', () => {
             const body = iframe.contents().find('body')//o Contests trás os filhos dos elementos
             cy.wrap(body).find('#tfield').type('funciona').should('have.value', 'funciona')
 
-            cy.on('window:alert', msg => {
+           /*  cy.on('window:alert', msg => {
             expect(msg).to.be.equal('Alert Simples') //Verifica se a mensagem exibida no alerta é igual a mensagem passada no parametro do equal
-            })
-            cy.wrap(body).find('#otherButton').click()
+            }) */
         })
     })
 
+    it('Testando Frame diretamente', () => {
+        cy.visit('https://www.wcaquino.me/cypress/frame.html')
+        cy.get('#otherButton').click()
+        
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Click OK!') //Verifica se a mensagem exibida no alerta é igual a mensagem passada no parametro do equal
+            })
+
+    })
+ 
     
 })
