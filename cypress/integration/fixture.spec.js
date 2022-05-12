b377d31ca575df5337b19b0c031f7a812803c925
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2022-05-12 12:04:05 -0300

    Fixture

--- cypress/integration/fixture.spec.js
+++ cypress/integration/fixture.spec.js
@@ -9,10 +9,12 @@ describe('Fixture teste', () => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
         cy.get('#formNome').type('Jeiseel')
         cy.get('#formSobrenome').type('Rodrigues')
-        cy.get('#formSexoMasc')
-        cy.get('#formComidaCarne')
-
-        cy.get('#formEsportes')
+        cy.get('[name=formSexo][value=M]').click()
+        cy.get('[name=formComidaFavorita][value=pizza]').click()
+        cy.get('#formEscolaridade').select('Superior')
+        cy.get('#formEsportes').select('Karate')
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
 
     })
 
